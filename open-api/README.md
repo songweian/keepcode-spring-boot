90c8d360feae7d6a85f177b0180997b3205b1172
weian404 <weian404@gmail.com>
2023-11-03 23:10:04 +0800

    micros

--- open-api/README.md
+++ open-api/README.md
@@ -1,1 +1,15 @@
 # API开放加签/验签
+
+## server
+
+### HttpServletRequest 验签
+
+## client
+
+## apache httpclient
+
+## feign client
+
+## okhttp client
+
+支持腾讯API加签/验签算法
---
+++ open-api/openapi-commons/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,10 @@
+package $org.opengear.openapi;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
