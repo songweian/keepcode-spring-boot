630f1ab70dd8fdf716fdd7d42666219ce29246de
weian404 <weian404@gmail.com>
2023-11-03 22:44:08 +0800

    micros

---
+++ dash-console/src/main/java/org/opengear/admin/DashConsoleApplication.java
@@ -0,0 +1,12 @@
+package org.opengear.admin;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashConsoleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DashConsoleApplication.class, args);
+    }
+}
